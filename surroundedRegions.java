9c012e703de9c8ffcae32ef0bf97c3c86bd7a1c8
Khushi Singh <khushiisingh2507@gmail.com>
2025-02-26 11:26:57 +0530

    Surrounded Regions

---
+++ surroundedRegions.java
@@ -0,0 +1,59 @@
+public class surroundedRegions {     //TC-O(m*n)+O((m*n)*4) ---> O(m*n), SC-O(m*n)
+    private char[][] adjM;
+
+    public surroundedRegions(char[][] mat){
+        this.adjM = mat;
+    }
+
+    public static void main(String args[]){
+        char[][] mat = {{'X', 'X', 'X', 'X'}, 
+                        {'X', 'O', 'X', 'X'}, 
+                        {'X', 'O', 'O', 'X'}, 
+                        {'X', 'O', 'X', 'X'}, 
+                        {'X', 'X', 'O', 'O'}};
+        surroundedRegions g = new surroundedRegions(mat);
+        int m = mat.length;
+        int n = mat[0].length;
+        int dx[] = {+1, -1, 0, 0};
+        int dy[] = {0, 0, +1, -1};
+        char[][] ans = mat;
+        boolean[][] vis = new boolean[m][n];
+        for(int j=0; j<n; j++){
+            if(!vis[0][j] && mat[0][j]=='O'){
+                dfs(0, j, vis, ans, dx, dy);
+            }
+            if(!vis[m-1][j] && mat[m-1][j]=='O'){
+                dfs(m-1, j, vis, ans, dx, dy);
+            }
+        }
+        for(int i=0; i<m; i++){
+            if(!vis[i][0] && mat[i][0]=='O'){
+                dfs(i, 0, vis, ans, dx, dy);
+            }
+            if(!vis[i][n-1] && mat[i][n-1]=='O'){
+                dfs(i, n-1, vis, ans, dx, dy);
+            }
+        }
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(!vis[i][j] && ans[i][j]=='O'){
+                    ans[i][j] = 'X';
+                }
+            }
+        }
+        System.out.println(ans);
+    }
+
+    public static void dfs(int i, int j, boolean[][] vis, char[][] ans, int[] dx, int[] dy){
+        int m = ans.length;
+        int n = ans[0].length;
+        vis[i][j] = true;
+        for(int d=0; d<4; d++){
+            int ni = i + dx[d];
+            int nj = j + dy[d];
+            if(ni>=0 && ni<m && nj>=0 && nj<n && !vis[ni][nj] && ans[ni][nj]=='O'){
+                dfs(ni, nj, vis, ans, dx, dy);
+            }
+        }
+    }
+}
